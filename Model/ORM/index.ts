5e20ca5b20a3575407497caa4ba483b217114349
Philippe Malo <philippe.malo21@gmail.com>
2023-02-24 21:25:23 -0500

    building model (orm + db)
    Dockerized Prisma
    Dockerized MongoDB
    
    Currently need to fix a bug with Prisma studio:
    Failed to commit changes: Invalid `t=YS(this),A=Object.create()` invocation

---
+++ Model/ORM/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { PrismaClient } from "./prisma/generated/prisma-client-js";
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.$connect();
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+app.get("/", (req, res) => {
+  res.send("PRISMA ORM!");
+});
+
+app.listen(4466, () => {
+  console.log("Server is running");
+});
