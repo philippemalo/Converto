0a5390731eadbeebc743c65ab8d49e98c3f23bdf
Philippe Malo <philippe.malo21@gmail.com>
2023-03-03 13:00:38 -0500

    Dockerized Vitejs Vue3 frontend (with tailwindcss and typescript)

--- Controller/index.ts
+++ Controller/index.ts
@@ -1,24 +1,6 @@
 import express from "express";
-import { PrismaClient } from "./prisma/generated/prisma-client-js";
 
 const app = express();
-
-const prisma = new PrismaClient();
-
-const main = async () => {
-  await prisma.$connect();
-  console.log("Prisma connection successful!");
-};
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
 
 app.get("/", (req, res) => {
   res.send("PRISMA ORM!");
---
+++ View/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Hello Vite + Vue 3 + TypeScript + TailwindCSS!</div>
+</template>
