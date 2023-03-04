cb2ccd35812daeaae5ec07ac27f9736db5f9858d
Philippe Malo <philippe.malo21@gmail.com>
2023-03-03 21:30:59 -0500

    OAuth2 User model with AuthDetails

--- Controller/index.ts
+++ Controller/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import { PrismaClient } from "./prisma/generated/prisma-client-js";
 
 const app = express();
+
+const prisma = new PrismaClient();
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 app.get("/", (req, res) => {
   res.send("PRISMA ORM!");
