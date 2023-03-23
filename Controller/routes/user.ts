11f4a50e4322b3650e26c01b2f4c80e6f3498444
Philippe Malo <philippe.malo21@gmail.com>
2023-03-23 01:00:19 -0400

    Prisma schema to implement user publications

--- Controller/routes/user.ts
+++ Controller/routes/user.ts
@@ -10,7 +10,28 @@ userRouter.get("/:username", async (req, res) => {
 
   if (!user) return res.status(404).json({ message: "User not found" });
 
-  return res.status(200).send({ username: user.username, name: user.name });
+  // get user's socials
+  const socials = await prisma.social.findMany({
+    where: { userId: user.id },
+  });
+
+  // get user's publications
+  const publications = await prisma.publication.findMany({
+    where: { userId: user.id },
+  });
+
+  return res.status(200).send({
+    username: user.username,
+    name: user.name,
+    location: user.location,
+    profilePicture: user.profilePicture,
+    about: user.about,
+    skills: user.skills,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+    socials: socials,
+    publications: publications,
+  });
 });
 
 export default userRouter;
